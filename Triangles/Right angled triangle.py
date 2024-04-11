9c06fd8e11ee19d723d7c51c5eb01be3ce04ce5e
AAKASH-2207 <90674945+AAKASH-2207@users.noreply.github.com>
2024-04-11 17:47:23 +0530

    Update Right angled triangle.py

--- Triangles/Right angled triangle.py
+++ Triangles/Right angled triangle.py
@@ -6,10 +6,10 @@ Creator: Aakash Sharma
 '''
 
 no_of_rows = int(input("enter the size of the Triangle: "))
-for rows in range(0, no_of_rows + 1):
-  for columns in range(0, i + 1):
-    print("* ",end="")
-  print("")
+for rows in range(0, no_of_rows + 1): 											# First for loop for the no. of rows to be in the triangle
+  for columns in range(0, i + 1):  												  # second loop for the steps or column length 
+    print("* ",end="") 				 			 												# printing the steps
+  print("") 																								# printing the next line
 
 '''
 expected Output:
