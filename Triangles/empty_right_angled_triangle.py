0522374204ad9f9524ffc3fb6c18b457552d7ddf
AAKASH-2207 <90674945+AAKASH-2207@users.noreply.github.com>
2024-05-18 16:18:37 +0530

    Create empty_right_angled_triangle.py

---
+++ Triangles/empty_right_angled_triangle.py
@@ -0,0 +1,26 @@
+''' 
+Code Description
+This code is to create a Empty left sided Right angled Triangle using Python language
+
+Creator: Aakash Sharma
+'''
+size = int(input("enter the size of the triangle: "))
+for i in range(size+1):
+  for j in range(i):
+    if(j==0 or j==i-1 or i == size):
+      print("* ",end="")
+    else:
+      print("  ",end ="")
+  print()
+
+'''
+EXPECTED OUTPUT
+enter the size of the triangle: 6
+
+* 
+* * 
+*   * 
+*     * 
+*       * 
+* * * * * *
+'''
