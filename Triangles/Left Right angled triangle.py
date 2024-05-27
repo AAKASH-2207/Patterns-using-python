39787a03c94f3a7da80c72f629c6732f3f6781d6
AAKASH-2207 <90674945+AAKASH-2207@users.noreply.github.com>
2024-05-27 22:34:08 +0530

    Update Left Right angled triangle.py

--- Triangles/Left Right angled triangle.py
+++ Triangles/Left Right angled triangle.py
@@ -6,8 +6,8 @@ Creator: Aakash Sharma
 '''
 
 no_of_rows = int(input("enter the size of the Triangle: "))
-for rows in range(0, no_of_rows + 1): 											# First for loop for the no. of rows to be in the triangle
-  for columns in range(0, i + 1):  												  # second loop for the steps or column length 
+for rows in range(0, no_of_rows): 											# First for loop for the no. of rows to be in the triangle
+  for columns in range(0, rows+1):  												  # second loop for the steps or column length 
     print("* ",end="") 				 			 												# printing the steps
   print("") 																								# printing the next line
 
@@ -19,6 +19,17 @@ enter the size of the Triangle: 5
 * * * 
 * * * * 
 * * * * * 
-* * * * * * 
-
 '''
+
+#CODE 2
+size = int(input("enter the size of triangle: "))
+for i in range(size):
+  print("* " * (i+1))
+'''
+EXPECTED OUTPUT
+enter the size of triangle: 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
